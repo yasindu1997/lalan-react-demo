ce9c1a36b8da0947643c14c9e532aef17c4e45fc
yasindu1997 <yasindusathsara@gmail.com>
2023-05-11 09:12:31 +0530

    style changes done

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import img1 from '../assets/game.jpg'
+import Form from 'react-bootstrap/Form';
 
 function HomePage() {
+
+  //state
+  const[email,setEmail] = useState('');
+  const[password,setPassword] = useState('');
+  const [data,setData] = useState([]);
+
+  const saveData = ()=>{
+    
+  }
+
   return (
     <div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={img1} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
 
-      <div className='main'>
-      <input type="email" placeholder='Enter username' className="form-control txt" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <input type="email" placeholder='Enter password' className="form-control txt" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <button style={{marginLeft:150}} type="button" className="btn btn-success">Login Here</button>
-      </div>
+      <br></br>
+      <Form.Control value={email} onChange={(e)=>{setEmail(e.target.value)}} style={{ width: 400,marginBottom:7 }} type="email" placeholder="Enter Email" />
+      <Form.Control style={{ width: 400,marginBottom:7 }} type="text" placeholder="Enter Password" />
+      <Button variant="warning" onClick={saveData}>Save Data</Button>
     </div>
   )
 }
